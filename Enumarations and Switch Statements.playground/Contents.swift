bf9ed3fd37957a48b229fb072b3b98c8e6318975
Anand Kulkarni <anandkulkarni@mwc-065238.dhcp.missouri.edu>
2018-09-07 17:36:12 -0500

    finished function

--- Enumarations and Switch Statements.playground/Contents.swift
+++ Enumarations and Switch Statements.playground/Contents.swift
@@ -2,3 +2,25 @@
 
 import UIKit
 
+enum StatusCode {
+    case success
+    case unauthorized
+    case forbidden
+    case notFound
+}
+
+func prettyPrint(status: StatusCode) -> String {
+    
+    switch status {
+    case .success:
+        return "200: Success"
+    case .unauthorized:
+        return "401: Unauthorized"
+    case .forbidden:
+        return "403: Forbidden"
+    case .notFound:
+        return "404: Not Found"
+    }
+}
+
+print(prettyPrint(status: StatusCode.success))
--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -7,14 +7,15 @@ func check(value: Int) -> String? {
         return nil
     }
     else {
+        
+        let numberString1: String? = check(value: 10)
+        let numberString2: String? = check(value: 0)
+        let numberString3: String? = check(value: -2)
+        
+        print(numberString1!)
+        print(numberString2 as Any)
+        print(numberString3 as Any)
+
         return String(value)
     }
 }
-
-let numberString1: String? = check(value: 10) // numberString should be "10"
-let numberString2: String? = check(value: 0) // numberString should be nil
-let numberString3: String? = check(value: -2) // numberString should be nil
-
-print(numberString1!)
-print(numberString2 as Any)
-print(numberString3 as Any)
